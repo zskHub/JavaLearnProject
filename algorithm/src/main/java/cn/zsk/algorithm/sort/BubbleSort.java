0120ca5b075be33f226ec9970dd4fc2617f1f5af
zsk <303316861@qq.com>
2021-06-01 23:00:50 +0800

    feat: 增加冒泡排序

--- algorithm/src/main/java/cn/zsk/algorithm/sort/BubbleSort.java
+++ algorithm/src/main/java/cn/zsk/algorithm/sort/BubbleSort.java
@@ -1,11 +1,44 @@
 package cn.zsk.algorithm.sort;
 
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 冒泡排序
  *
  * @Author : zsk
  * @CreateTime : 2021-06-01   17:31
  */
+@Slf4j
 public class BubbleSort {
 
+    public static void doBubbleSort(Integer[] arr){
+        if (null == arr || arr.length < 0){
+            log.info("冒泡排序，传入数组为空");
+            return ;
+        }
+        if (arr.length == 1){
+            log.info("冒泡排序，传入数组长度为1.");
+            return ;
+        }
+
+        for (int i = 0; i < arr.length - 1; i++){
+            for (int j = 0; j < arr.length - 1 - i; j ++){
+                if (arr[j] > arr[j + 1]){
+                    Integer temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j +1] = temp;
+                }
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Integer [] arr = {3,5,4,2,8,2,10};
+        log.info("冒泡排序，原始数组：{}", JSON.toJSONString(arr));
+        BubbleSort.doBubbleSort(arr);
+        log.info("冒泡排序，结果数组：{}", JSON.toJSONString(arr));
+
+    }
 }
